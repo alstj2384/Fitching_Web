1240832f819a5cb81cea3995e86e8823569572e4
jongbinchoi <jongbin985@naver.com>
2024-07-15 15:29:50 +0900

    LikeLion2024 hackathon

---
+++ src/main/java/ssamppong/fitchingWeb/FitchingWebApplication.java
@@ -0,0 +1,13 @@
+package ssamppong.fitchingWeb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitchingWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitchingWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ssamppong/fitchingWeb/FitchingWebApplicationTests.java
@@ -0,0 +1,13 @@
+package ssamppong.fitchingWeb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitchingWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
