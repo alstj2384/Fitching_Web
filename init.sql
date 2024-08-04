eae09c75a04eff03ffea8dbbc3f8b9e2a509f301
jongbinchoi <jongbin985@naver.com>
2024-08-04 16:14:45 +0900

    [fix : init.sql 수정]

--- init.sql
+++ init.sql
@@ -1,4 +1,4 @@
-INSERT INTO USERS ( email, member_level, current_points, completed_stretchings,  tier, name, role, password)
-VALUES ('admin', 0, 0 ,0 ,'Bronze', 'admin', 'USER', '$2a$10$.8ALtrQsc17C4lLU2xRDGejsJnm3ZqBZ3UIiSlnj2geP8uD4gu93S');
-INSERT INTO BODY_PARTS ( part_name, count, user_id) VALUES ('head', 0, 1);
-INSERT INTO BODY_PARTS ( part_name, count, user_id) VALUES ('torso', 0, 1);
+CREATE DATABASE IF NOT EXISTS fitchingdb;
+CREATE USER IF NOT EXISTS 'fitchingmaster'@'%' IDENTIFIED BY '12341234!';
+GRANT ALL PRIVILEGES ON fitchingdb.* TO 'fitchingmaster'@'%';
+FLUSH PRIVILEGES;
